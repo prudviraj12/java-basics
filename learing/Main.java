5224abc851a83960d623a03dbf40557e6f55c35a
prudviraj12 <prudvirajreddy98@gmail.com>
2026-07-11 21:32:44 +0530

    l2

--- learing/Main.java
+++ learing/Main.java
@@ -1,5 +1,16 @@
-public class Main {
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+
+// }
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int n = 10;
+
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
     }
 }
