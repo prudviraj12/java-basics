62e83edc2f925d95ba6bad038f9cf7160f2cd40b
prudviraj12 <prudvirajreddy98@gmail.com>
2026-07-19 22:39:26 +0530

    day2

--- learing/oops/basic_oops.java
+++ learing/oops/basic_oops.java
@@ -14,16 +14,8 @@ class office {
 
 }
 
-// public class basic_oops {
+// public c
 
-// public static void main(String[] args) {
-// office branch1 = new office();
-// branch1.off_name = "reddy";
-// branch1.mem_name = 5;
-// System.out.println(branch1.off_name);
-// System.out.print(branch1.mem_name);
-// }
-// }
 public class basic_oops {
     public static void main(String[] args) {
         office branch1 = new office("reddy", 6);
