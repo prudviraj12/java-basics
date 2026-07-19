d8acceb5b1112e442c9fcb38c769cb3b1f9604ed
prudviraj12 <prudvirajreddy98@gmail.com>
2026-07-19 13:00:09 +0530

    xerox_shop

--- learing/oops/basic_oops.java
+++ learing/oops/basic_oops.java
@@ -1,16 +1,21 @@
 package oops;
 
+// public class xer
+
 class office {
     String off_name;
     int mem_name;
 
     public office(String s, int n) {
         System.out.println(s + " " + n);
-
+        off_name = s;
+        mem_name = n;
     }
+
 }
 
 // public class basic_oops {
+
 // public static void main(String[] args) {
 // office branch1 = new office();
 // branch1.off_name = "reddy";
@@ -22,5 +27,7 @@ class office {
 public class basic_oops {
     public static void main(String[] args) {
         office branch1 = new office("reddy", 6);
+        System.out.println(branch1.off_name);
+        System.out.println(branch1.mem_name);
     }
 }
---
+++ learing/oops/xerox.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class xerox {
+    String branch_name;
+    int emp;
+    int sal;
+
+    public xerox() {
+        System.out.println("constructor executed");
+    }
+}
+    public class {
+    public static void main(String[] args) {
+        xerox b1 = new xerox();
+    }}
