84f4d764081504007da84e797889563141ac52ca
prudviraj12 <prudvirajreddy98@gmail.com>
2026-07-26 16:47:04 +0530

    random_num_game

--- learing/oops/Random.java
+++ learing/oops/Random.java
@@ -1,13 +1,12 @@
 import java.util.Random;
-
 public class random {
     public static void main(String[] args) {
         Random random = new Random();
-
         int num = random.nextInt(100) + 1;
         int num1 =random.nextInt(1,100);
+        int nums2 =random.nextInt(2,3);
+        System.out.println(nums2);
         System.out.println(num1);
-
         System.out.println(num);
     }
 }
