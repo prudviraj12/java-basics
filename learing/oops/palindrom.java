ee197f9b973a184a712e7a6e15f4d400f95bf32c
prudviraj12 <prudvirajreddy98@gmail.com>
2026-07-30 21:44:50 +0530

    daily

---
+++ learing/oops/palindrom.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 121;
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            reverse = reverse * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (original == reverse)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
